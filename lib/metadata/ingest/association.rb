a20f34c75bbd5ea89ce62ed4ddc5ab39e877c23d
Jeremy Echols <jechols@uoregon.edu>
2013-12-30 16:40:45 -0800

    Write up docs on ingest association class
    Probably more for myself than others, this explains the concept of the
    "internal" attribute in depth as well as adding a basic explanation of
    the other attributes

--- lib/metadata/ingest/association.rb
+++ lib/metadata/ingest/association.rb
@@ -3,7 +3,31 @@ require "metadata/ingest/form_backer"
 
 module Metadata
   module Ingest
-    # Simple association class for ingest form's nested data
+    # Simple association class for ingest form's nested data.  Attributes:
+    # * `group`: Human interface element for grouping similar data - titles,
+    #   subjects, etc
+    # * `type`: More specific data for what this data means within a group -
+    #   main title, alternate title, etc
+    # * `value`: Human-friendly value
+    # * `internal`: Internal representation of the value, if applicable
+    #
+    # Type and value must always be present for a valid object.  Group isn't
+    # strictly required, but it won't make sense for translation without a
+    # group.
+    #
+    # Internal data is optional, and only used for complex data that requires a
+    # human-readable component.  For instance, on a web form an autocomplete
+    # could show the user "Food industry and trade" when "food" was typed in.
+    # On selection and save, the form would send "Food industry and trade" as
+    # `value`, but "http://id.loc.gov/authorities/subjects/sh85050282" would be
+    # sent as `internal`.  Using the standard form-to-attribute translator, the
+    # object would never see the human-friendly term and only receive the URI.
+    #
+    # When converting from object to form (for editing or even display), the
+    # translator would need to be able to recognize that
+    # "http://id.loc.gov/authorities/subjects/sh85050282" isn't a
+    # human-friendly value so it could put that into `internal` and put "Food
+    # industry and trade" into `value`.
     class Association < FormBacker
       include ActiveModel::Validations
 
