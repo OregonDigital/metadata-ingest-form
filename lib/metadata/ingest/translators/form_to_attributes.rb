0d748f3937b7a432eeae4e1fdece5659a1ce98c5
Jeremy Echols <jechols@uoregon.edu>
2014-01-22 17:51:14 -0800

    Refactor FormToAttributes for easier overriding
    Changes the internal conversion process to store association object
    information longer, allowing overridden methods to have more context
    for converting and otherwise handling data

--- lib/metadata/ingest/translators/form_to_attributes.rb
+++ lib/metadata/ingest/translators/form_to_attributes.rb
@@ -41,10 +41,11 @@ module Metadata::Ingest::Translators
     def to(output)
       build_translated_attribute_hash
 
-      for attr, val in @attributes
+      for attr, associations in @attributes
         objects = attr.to_s.split(".")
         attribute = objects.pop
-        objects.reduce(output, :send).send("#{attribute}=", val)
+        object = objects.reduce(output, :send)
+        store_associations_on_object(object, attribute, associations)
       end
     end
 
@@ -57,49 +58,60 @@ module Metadata::Ingest::Translators
       end
     end
 
-    # Converts a single association (from @form) into a key-value pair in @attributes.  If the
-    # association has `internal`, that field will be the value, otherwise `value` is used.
-    def translate_association(assoc)
+    # Extracts the attribute definition for a given association
+    def attribute_lookup(assoc)
       group_data = @map[assoc.group.to_sym]
-      return unless group_data
+      return nil unless group_data
 
       attribute = group_data[assoc.type.to_sym]
-      return unless attribute
-
-      # Make sure we properly handle destroyed values by forcing them to nil.
-      # We keep their state up to this point (and avoid modifying the "live"
-      # object) because the user may be using the prior value for something.
-      if assoc.marked_for_destruction?
-        assoc = assoc.dup
-        assoc.internal = nil
-        assoc.value = nil
-      end
-
-      # This is necessary for web forms where "internal" => "" is a very
-      # expected situation we have to handle.  I'm not sure there are valid
-      # situations where an internal value of "" is actually worthy of
-      # preservation when the "value" field has data.
-      value = assoc.internal.blank? ? assoc.value : assoc.internal
-      store_attribute_value(attribute, value)
+      return attribute
     end
 
-    # Adds the given value to the attribute in @attributes.  If there are no existing values,
-    # stores the value as-is.  If the value is an array, the new value is added.  Otherwise, the
-    # old value is put into an array with the new value.
-    def store_attribute_value(attribute, value)
+    # Maps an association to the attribute its data will be tied
+    def translate_association(assoc)
+      attribute = attribute_lookup(assoc)
+      return unless attribute
+
+      # Make sure we properly handle destroyed values by forcing them to an
+      # empty association.  We keep their state up to this point because the
+      # caller may be using the prior value for something.
+      if assoc.marked_for_destruction?
+        assoc = Metadata::Ingest::Association.new
+      end
+
+      add_association_to_attribute_map(attribute, assoc)
+    end
+
+    # Adds the given association to an array of associations for the given
+    # attribute
+    def add_association_to_attribute_map(attribute, assoc)
       current = @attributes[attribute]
 
-      # If there's already a value, we can safely ignore nils - there is
-      # currently no use case for storing a nil object alongside valid data.  A
-      # value of nil just means the item was marked for destruction, and needs
-      # to be set to nil if no other values are present.
-      return if current && value.nil?
+      # If there's already a value, we can safely ignore the empty association
+      return if current && assoc.blank?
 
       case current
-        when nil    then @attributes[attribute] = value
-        when Array  then @attributes[attribute].push(value)
-        else             @attributes[attribute] = [current, value]
+        when nil    then @attributes[attribute] = [assoc]
+        when Array  then @attributes[attribute].push(assoc)
       end
+    end
+
+    # Stores all association data on the object at the given attribute.
+    # Associations with internal data use that instead of value.  If only one
+    # association is present, it is extracted from the array and stored as-is.
+    def store_associations_on_object(object, attribute, associations)
+      values = associations.collect do |assoc|
+        assoc.internal.blank? ? assoc.value : assoc.internal
+      end
+
+      # Clean up values
+      values.compact!
+      case values.length
+        when 0 then values = nil
+        when 1 then values = values.first
+      end
+
+      object.send("#{attribute}=", values)
     end
   end
 end
